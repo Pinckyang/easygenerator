a3c3f9093594105c6feccd57e0e8ea859f52c126
pinck@qq.com <12264845yang>
2016-05-09 22:24:44 +0800

    add custom list

---
+++ EasyGenerator/EasyGenerator.Studio/Utils/ContextObjectList.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using EasyGenerator.Studio.Model;
+using System.Runtime.Serialization;
+using System.Xml;
+using System.Xml.Schema;
+
+namespace EasyGenerator.Studio.Utils
+{
+
+    [Serializable()]
+    public class ContextObjectList<T> : List<T> where T : ContextObject
+    {
+        public ContextObject Owner
+        {
+            get;
+            set;
+        }
+
+        public ContextObjectList()
+        {
+
+        }
+        public ContextObjectList(ContextObject owner)
+        {
+
+        }
+        public void Add(T value)
+        {
+            value.Owner = Owner;
+            base.Add(value);
+        }
+    }
+
+
+}
